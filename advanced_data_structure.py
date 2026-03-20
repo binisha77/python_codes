dbad1e6ef5265cddd2528b00b4bddec46e5c1c1b
Binisha Khadka <khadkabinisha48@gmail.com>
2026-03-20 12:40:08 +0545

    feat : advanced DSA stack and queue

---
+++ advanced_data_structure.py
@@ -0,0 +1,17 @@
+#last in first out
+
+
+stack = []
+
+stack.append("Type A")
+stack.append("Type B")
+stack.append("Delete Word")
+
+print("Actions:", stack)
+
+undo = stack.pop() 
+print("Undo:", undo)
+print("Remaining:", stack)
+
+
+# Queue FIFO first in first out 
