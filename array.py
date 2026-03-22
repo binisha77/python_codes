746e7c96265e9e2e26e9195f863d932d85a87e43
Binisha Khadka <khadkabinisha48@gmail.com>
2026-03-22 08:32:43 +0545

    feat: python array example

---
+++ array.py
@@ -0,0 +1,17 @@
+# python arrays usually use lists
+numbers = [10, 20, 30, 40]
+
+#Loop through array:
+for num in numbers:
+   print(num)
+
+
+#sum
+numbers = [10, 20, 30, 40]
+
+total = 0
+
+for n in numbers:
+   total = total + n
+
+print("Total:", total)
